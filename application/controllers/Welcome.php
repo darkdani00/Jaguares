cded3b596d810e989eb522a7fa756ab3ecb2bf95
santiago <santiagocastanonarvizu@gmail.com>
2020-09-16 14:46:41 -0500

    archivos bootstrap, fontawesome, layout para las navbar

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('includes/header');
+		$this->load->view('includes/red_bar');
+		$this->load->view('includes/black_bar');
+		// $this->load->view('welcome_message');
+		$this->load->view('includes/footer');
 	}
 }
---
+++ application/views/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="<?=base_url("resources/bootstrap/css/bootstrap.min.css");?>">
+    <!-- Font awesome -->
+    <link href="<?=base_url("resources/css/style.css");?>" rel="stylesheet">
+    <link href="<?=base_url("resources/fontawesome/css/all.css");?>" rel="stylesheet">
+</head>
+<body>
+    
