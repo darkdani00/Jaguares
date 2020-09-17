07cee39414c2bc3ce3e3494782b097c8507dcafe
darkdani00 <66800529+darkdani00@users.noreply.github.com>
2020-09-17 12:19:08 -0500

    Logo

--- application/views/includes/black_bar.php
+++ application/views/includes/black_bar.php
@@ -1,9 +1,9 @@
 <!-- navbar start -->
 <nav class="navbar navbar-expand-lg navbar-dark stick-bar black_bar_size" style="background-color: #000000;">
     <a class="navbar-brand ml-5" href="#">
-        <img src="<?=base_url("resources/img/logo_bg.jpg");?>" width="30" height="30" alt="" loading="lazy">
+        <img src="<?=base_url("resources/img/logo.jpg");?>" width="60" height="60" alt="" loading="lazy">
     </a>
-    
+
     <div class="navbar-collapse collapse w-100 order-3 dual-collapse2 mr-5">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
@@ -12,8 +12,15 @@
             <li class="nav-item active">
                 <a class="nav-link" href="#">ACERCA DE</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">INICIAR SESION</a>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    INICIAR SESION
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Alumno</a>
+                    <a class="dropdown-item" href="#">Profesor</a>
+                </div>
             </li>
         </ul>
     </div>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jaguares</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" type="text/css" href="<?=base_url("resources/bootstrap/css/bootstrap.min.css");?>">
     <!-- Font awesome -->
