3279452bcfd4830935165ac80b5a8811515c9ada
santiago <santiagocastanonarvizu@gmail.com>
2020-09-24 23:59:38 -0500

    navegacion mejorada

--- application/views/includes/navegation_log.php
+++ application/views/includes/navegation_log.php
@@ -55,28 +55,6 @@
             </div>
         </nav>
         <div id="content">
-            <section>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, suscipit repellendus autem dignissimos quo alias cupiditate quae praesentium hic maiores laudantium illum ipsa in dolore, nulla obcaecati, nam quidem amet.</h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-
-
-
-    </div>
     
-</div>
+
 
