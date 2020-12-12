4c13a21c4aa4ad27cdb5418e8ac68a629258b1eb
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2020-12-12 14:55:29 -0600

    facebook feed

--- application/views/view/Acerca_page.php
+++ application/views/view/Acerca_page.php
@@ -1,45 +1,55 @@
-<br>
-<div class="row">
-    <div class="col-1"></div>
-    <div class="col-md-5 col-lg-5 col-sm-12">
-        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="<?=base_url("resources/img/fondo1.JPG");?>" widht="350" height="450" class="d-block w-100"
-                        alt="fondo1">
-                </div>
-                <div class="carousel-item">
-                    <img src="<?=base_url("resources/img/fondo2.JPG");?>" widht="350" height="450" class="d-block w-100"
-                        alt="fondo2">
-                    <div class="carousel-caption d-none d-md-block">
+<div class="container align-items-center">
+    <div class="row mt-5 justify-content-center">
+        <div class="col">
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?=base_url("resources/img/fondo1.JPG");?>" widht="350" height="450"
+                            class="d-block w-100" alt="fondo1">
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="<?=base_url("resources/img/fondo3.JPG");?>" widht="350" height="450" class="d-block w-100"
-                        alt="fondo3">
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="carousel-item">
+                        <img src="<?=base_url("resources/img/fondo2.JPG");?>" widht="350" height="450"
+                            class="d-block w-100" alt="fondo2">
+                        <div class="carousel-caption d-none d-md-block">
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?=base_url("resources/img/fondo3.JPG");?>" widht="350" height="450"
+                            class="d-block w-100" alt="fondo3">
+                        <div class="carousel-caption d-none d-md-block">
 
+                        </div>
                     </div>
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+        </div>
+        <div class="col">
+            <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FJaguares-TKD-Quer%25C3%25A9taro-288080018943&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
     </div>
-    <div class="col-md-6 col-lg-6 col-sm-12">
-        <br>
-        <h5>¿Quiénes somos?</h5>
-        <p>Somos una escuela que lleva # años dando clases teniendo experienza en.....</p>
+    <div class="row text-center mt-5">
+        <div class="col-12">
+            <h5>¿Quiénes somos?</h5>
+        </div>
+        <div class="col-12">
+            <p>Somos una escuela que lleva # años dando clases teniendo experienza en.....</p>
+        </div>
     </div>
 </div>
