6b673e547ac65b9d3ef471d9425f2badda9c2ffd
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-01-26 19:22:33 -0600

    email config file

---
+++ application/config/email.php
@@ -0,0 +1,11 @@
+<?php 
+ 
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_user'] = 'santiagodev12@gmail.com';
+$config['smtp_pass'] = 'santiagot45456465';
+$config['smtp_port'] = 465;
+ 
+$config['charset'] = 'utf-8';
+$config['mailtype'] = 'html';
+$config['newline'] = "\r\n"; 
